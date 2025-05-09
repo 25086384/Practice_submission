9a162f1c1705cd381151d35e273c38b2b0099f6c
25086384 <25086384@mvula.univen.ac.za>
2025-05-09 15:48:45 +0200

    Insurance.java

--- Insurance.java
+++ Insurance.java
@@ -17,16 +17,15 @@ public class Insurance {
         System.out.print("Enter your birth year: ");
         int birthYear = scanner.nextInt();
 
-        int premium = calculatePremium(currentYear, birthYear);
-        System.out.println("Your insurance premium is: $" + premium);
-        
+        double premium = calculatePremium(currentYear, birthYear);
+
+        System.out.println("Your annual policy premium is: $" + premium);
     }
 
-    
     public static int calculatePremium(int currentYear, int birthYear) {
         int age = currentYear - birthYear;
-        int ageDecade = age / 10; 
-        return (ageDecade + 15) * 20;
+        int decade = age / 10;
+        int premium = (decade + 15) * 20;
+        
+        return premium;
     }
-}
-
